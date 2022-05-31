7405ad45a0a9cec8bba8f2e4ff6833bac284dd1f
Dheeraj TP <45311336+dheerajtp@users.noreply.github.com>
2022-05-31 10:47:11 +0530

    Full Project

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { Container, Box } from "@mui/material";
+import { useState } from "react";
+import "./App.css";
+import { userValidationSchema } from "./Validation/FormValidation";
+
+function App() {
+  const [error, setError] = useState(null);
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const inputHandler = (e) => {
+    setError(null);
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+  const createUser = async (e) => {
+    e.preventDefault();
+    try {
+      await userValidationSchema.validate(input);
+    } catch (Err) {
+      const { errors } = Err;
+      setError(errors);
+    }
+  };
+  return (
+    <Container>
+      <form onSubmit={createUser}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            margin: "1rem",
+          }}
+        >
+          <input
+            type="text"
+            placeholder="name"
+            name="name"
+            value={input?.name}
+            onChange={inputHandler}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            value={input?.email}
+            onChange={inputHandler}
+          />
+          <input
+            type="text"
+            placeholder="password"
+            name="password"
+            value={input?.password}
+            onChange={inputHandler}
+          />
+          <button>Submit</button>
+          {error && <p style={{ color: "red", text: "center" }}>{error}</p>}
+        </Box>
+      </form>
+    </Container>
+  );
+}
+
+export default App;
